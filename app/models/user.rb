6e2ed8bd9cd642816ab9b745c37afed62c602f24
Andrew Krasnoff <andrew.krasnov1979@gmail.com>
2022-03-17 01:42:41 +0200

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :rememberable, :validatable, :recoverable
+  # :confirmable, :lockable, :timeoutable, :trackable, :recoverable and :omniauthable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
   
   validates :phone, presence: true, uniqueness: true, format: { with: /\d{10}/ }
 end
